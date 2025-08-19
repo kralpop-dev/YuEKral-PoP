19b71dce7cbc61f867372fe4d91e202ee58ede20
DOREMI-MUSICA <joshuahdays@gmail.com>
2025-08-19 02:48:24 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YuEKral",
+    version="0.1.0",
+    description="YuEKral Music/Audio processing tools",
+    author="kralpop-dev",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.7",
+)
